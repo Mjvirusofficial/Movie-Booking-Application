b169f3c50e1db2bca81c1e95a3b4502017368225
mjvirusofficial <deepakravidas565@gmail.com>
2023-02-25 18:08:59 +0530

    signup only

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Footer from './Feature/Movie/Footer/Footer';
-import Home from './Feature/Movie/Home/Home.jsx/Home';
-import { Signin } from './Feature/User/Signin/Signin';
+// import Home from './Feature/Movie/Home/Home.jsx/Home';
+// import { Signin } from './Feature/User/Signin/Signin';
 import { Signup } from './Feature/User/Signup/Signup';
 
 
 function App() {
   return (
     <div className="App">
+    <Signup/>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      {/* <Route path='/' element={<Home/>}/> */}
       <Route path='/signup' element={<Signup/>}/>
-      <Route path='/signin' element={<Signin/>}/>
+      {/* <Route path='/signin' element={<Signin/>}/> */}
     </Routes>
     <Footer/>
     </div>
